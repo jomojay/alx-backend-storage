7212d2448d119cccfe2d9676a20e932862b51356
jomojay <djdrjay001@gmail.com>
2024-07-17 20:28:13 +0100

    fixed

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -20,12 +20,12 @@ def data_cacher(method: Callable) -> Callable:
         '''The wrapper function for caching the output.
         '''
         redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
+        result = redis_store.get(f'cached:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
         redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
+        redis_store.setex(f'cached:{url}', 10, result)
         return result
     return invoker
 
